aa83bfd15ba0d5bd7317120ead577b2df1dc68b2
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2025-10-04 22:20:19 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,3 +1,12 @@
+  <!--- Caching managing  --->
+
+  if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch((err) => console.error("Service Worker registration failed:", err));
+}
+
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
